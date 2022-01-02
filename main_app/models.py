c5e0865f4f280ddc08a2f8815fb1a01f0a44f409
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-01-02 17:45:59 -0500

    stub up actual model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Show(models.Model):
+  title = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  runtime = models.IntegerField()
+  episodes = models.IntegerField()
+  complete = models.BooleanField()
