21e8635f4978748b72698e4025f6a1afb8ceeb34
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-01-02 14:44:27 -0500

    resize image

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello there I heard you have a backlog of movies and shows you need yo catch up on!</h1>')
+  return HttpResponse('<h1>Hello there I heard you have a backlog of movies and shows you need to catch up on!</h1>')
 
 def about(request):
   return render(request, 'about.html')
