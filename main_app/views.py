a2ebbbca6635e08fd34516a294a9d81bb6d1bf13
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-01-02 13:30:39 -0500

    basic stub ups

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+  return HttpResponse('<h1>Hello there I heard you have a backlog of movies and shows you need yo catch up on!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About this page and project: Coming soon!</h1>')
