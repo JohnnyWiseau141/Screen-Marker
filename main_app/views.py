ede042c7f3366eff25b1288f270dfc642b2b1315
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-01-03 08:46:09 -0500

    details page functionality

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,7 @@ def about(request):
 def shows_index(request):
   shows = Show.objects.all()
   return render(request, 'shows/index.html', { 'shows': shows })
+
+def shows_detail(request, show_id):
+  show = Show.objects.get(id=show_id)
+  return render(request, 'shows/detail.html', { 'show': show })
